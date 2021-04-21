f299bc925d0ac4129ba0e3a4fda937c87570f2a2
Daniel Lewis <daniel.lewis@protonmail.com>
2021-04-21 14:43:47 -0600

    added login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,8 @@ const { Post, User, Comment, Vote } = require("../models");
 
 // get all posts for homepage
 router.get("/", (req, res) => {
+  console.log(req.session);
+
   console.log("======================");
   Post.findAll({
     attributes: [
@@ -45,6 +47,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
   res.render("login");
 });
 
