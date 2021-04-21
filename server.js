058c46548f75dd26ff8026a8e7270cc5bf8e0fa1
Daniel Lewis <daniel.lewis@protonmail.com>
2021-04-21 14:15:44 -0600

    work on login page

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sequelize = require('./config/connection');
-
 const hbs = exphbs.create({});
+const sess = {
+  secret: "secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(require('./controllers/'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
